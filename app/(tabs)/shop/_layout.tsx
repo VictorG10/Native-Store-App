5ffc76bd65479a3321209a312102496f51abfe5a
VictorG10 <victorgbadebo10@gmail.com>
2025-07-22 18:55:39 +0100

    adding status bar

--- app/(tabs)/shop/_layout.tsx
+++ app/(tabs)/shop/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { StatusBar } from "react-native";
 
 const ShopLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="[id]" options={{ title: "Product Details" }} />
-    </Stack>
+    <>
+      <StatusBar />
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="[id]" options={{ title: "Product Details" }} />
+      </Stack>
+    </>
   );
 };
 
